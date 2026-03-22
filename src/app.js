100ddac3ad829001f5ab26c5f0d2db152ebfc4d0
Chathumi21 <chathumiisurika0221@gmail.com>
2026-03-22 11:32:37 +0530

    Created Swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const YAML = require("js-yaml");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
@@ -12,6 +16,10 @@ app.use(express.json());
 
 const orderRoutes = require('./routes/orderRoutes');
 app.use('/api', orderRoutes);
+
+const openapiPath = path.join(__dirname, "..", "openapi.yaml");
+const swaggerDocument = YAML.load(fs.readFileSync(openapiPath, "utf8"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
     res.send("Order Service Running");
